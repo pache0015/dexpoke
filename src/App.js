bc5f8ba1d4382534b3255092af334b5d15efee35
pache0015 <pache.facundo@gmail.com>
2021-09-07 20:20:19 -0300

    Usando react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import PokesPage from "./pages/pokesPage";
+import PokePage from "./pages/pokePage";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PokesPage/>
-    </div>
+      <Router>
+          <div>
+              <Switch>
+                  <Route path="/">
+                      <PokesPage/>
+                  </Route>
+                  <Route path="/poke/:id">
+                      <PokePage/>
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 

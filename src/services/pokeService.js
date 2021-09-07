57a3f9a5ff398910762f8a88f2e9e73821e8b580
pache0015 <pache.facundo@gmail.com>
2021-09-07 14:11:17 -0300

    Service para traer todos los pokes

---
+++ src/services/pokeService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default function getAllPokeService(){
+    return axios.get('https://pokeapi.co/api/v2/pokemon?limit=1500')
+        .then(response => {
+            return response.data.results;
+        })
+        .catch((error) => {
+            console.log('error ' + error);
+            return error;
+        });
+}

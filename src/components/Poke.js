73025ceeb916d36e3376923008f30508958ca8b3
pache0015 <pache.facundo@gmail.com>
2021-09-08 23:06:42 -0300

    usando service de un poke para traer sus imagenes

--- src/components/Poke.js
+++ src/components/Poke.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import getAllPokeService from "../services/getAllPokes";
+import getOnePokeService from "../services/getPoke";
 
 export default function Poke({name, url}){
+    const [id, setId] = useState()
+    const [imgFront, setImgFront] = useState()
+    const [imgBack, setImgBack] = useState()
+    const [imgOther, setImgOther] = useState()
+
+    useEffect(() =>{
+        getOnePokeService(url).then(data =>{
+            setId(data.id)
+            setImgFront(data.sprites.front_default)
+            setImgBack(data.sprites.back_default)
+            setImgOther(data.sprites.other.dream_world.front_default)
+        })
+    }, [url])
     return(
-        <h3>
-            <a href={url}>
-                {name}
-            </a>
-        </h3>
+        <Link className="poke" to={`/poke/${url}`}>
+            <img src={imgOther}/>
+            <h3>{name}</h3>
+        </Link>
     )
 }
